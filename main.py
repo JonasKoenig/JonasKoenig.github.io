a6c20e30813c1cf9f766d03e085ac0185db5444a
JonasKoenig <jonas-koenig@gmx.net>
2026-02-21 09:39:12 +0100

    test var

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+import psycopg2
+
+print(DATABASE_URL)
+print(os.environ['DATABASE_URL'])
+
+conn = psycopg2.connect(
+        host="localhost",
+        database="flask_db",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
